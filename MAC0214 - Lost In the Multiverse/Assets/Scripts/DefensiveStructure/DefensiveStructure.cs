234fb9bd992ad3b191dc90481fb2d792df96fa83
André Gustavo Nakagomi Lopez <andregnl@usp.br>
2023-09-18 15:11:51 -0300

    Add DefensiveStructure prototype
    Signed-off-by: André Gustavo Nakagomi Lopez <andregnl@usp.br>

---
+++ MAC0214 - Lost In the Multiverse/Assets/Scripts/DefensiveStructure/DefensiveStructure.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction {LEFT, RIGHT}
+
+public class DefensiveStructure : MonoBehaviour
+{
+	[SerializeField] GameObject flippableObjects;
+	[SerializeField] protected Projectile projectile;
+	[SerializeField] protected float attackDelay = 2.0f;
+	[SerializeField] protected float damage = 10.0f;
+
+	protected float currentTime = 0.0f;
+	protected Direction thisDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExecuteDefensiveBehavior();
+    }
+
+	public void SetDirection(Direction direction)
+	{
+		thisDirection = direction;
+
+        Quaternion newRot = new Quaternion();
+
+		if (thisDirection == Direction.LEFT)
+			newRot.y = -180.0f;
+		else
+			newRot.y = 0.0f;
+
+        flippableObjects.transform.rotation = newRot;
+	}
+
+    public virtual void ExecuteDefensiveBehavior()
+    {
+
+    }
+}
