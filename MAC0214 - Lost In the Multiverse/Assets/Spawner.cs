950c5c92700f7ad5e173999524b038f029e3969c
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2023-10-06 17:39:08 -0300

    Add Spawner Script basics

---
+++ MAC0214 - Lost In the Multiverse/Assets/Spawner.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+    
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+
+    }
+
+    [SerializeField] private Wave[] waves;
+    private int nextWave = 0;
+
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float waveCountdown;
+
+    void Start() {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update() {
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else{
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave _wave){
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy){
+
+    }
+
+}
