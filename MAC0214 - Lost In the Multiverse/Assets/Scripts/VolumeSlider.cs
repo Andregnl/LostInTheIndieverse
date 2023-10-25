c804b7085c661b6683efa87d26c13a1557dd3ea0
lincolnyuji <63720047+Lincoln-Yuji@users.noreply.github.com>
2023-10-25 18:17:07 -0300

    Add Volume Slider

---
+++ MAC0214 - Lost In the Multiverse/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] private AudioMixer objAudioMixer;
+    [SerializeField] private string audioMixerParameter;
+
+    private Slider slider;
+
+    void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        if (!slider)
+        {
+            Debug.LogError("Error, this object does not have a Slider component!");
+        }
+    }
+
+    public void ChangeVolume(float sliderValue)
+    {
+        if (audioMixerParameter == "SFX")
+        {
+            GameManager.Instance.settingsData.SFXVolume = sliderValue;
+        }
+        else if (audioMixerParameter == "music")
+        {
+            GameManager.Instance.settingsData.musicVolume = sliderValue;
+        }
+        else // Master volume
+        {
+            GameManager.Instance.settingsData.masterVolume = sliderValue;
+        }
+
+        objAudioMixer.SetFloat(audioMixerParameter, Mathf.Log10(sliderValue) * 20);
+    }
+}
