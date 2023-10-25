ed5e8dc02636735c5339c15217e796d49fb4ee84
lincolnyuji <63720047+Lincoln-Yuji@users.noreply.github.com>
2023-10-25 18:16:43 -0300

    Add Settings Data

---
+++ MAC0214 - Lost In the Multiverse/Assets/Scripts/SettingsData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SettingsData
+{
+    public float SFXVolume = 1.0f;
+    public float musicVolume = 1.0f;
+    public float masterVolume = 1.0f;
+}
+
+public class PersistentSettingsData : MonoBehaviour
+{
+    SettingsData settingsData = new SettingsData();
+
+    PersistentSettingsData(SettingsData settingsData)
+    {
+        this.settingsData = settingsData;
+    }
+}
