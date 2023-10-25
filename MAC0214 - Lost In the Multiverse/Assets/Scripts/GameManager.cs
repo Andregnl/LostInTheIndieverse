dc71d4c4702b474fb417c5b1575c8725a2f464cc
lincolnyuji <63720047+Lincoln-Yuji@users.noreply.github.com>
2023-10-25 18:50:01 -0300

    Make sliders change mixers by exposed params

--- MAC0214 - Lost In the Multiverse/Assets/Scripts/GameManager.cs
+++ MAC0214 - Lost In the Multiverse/Assets/Scripts/GameManager.cs
@@ -9,12 +9,9 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject startPosObj;
     public static GameManager Instance { get; private set; }
-    public GameObject playerObject;
     public SettingsData settingsData = new SettingsData();
 
     private bool isInstance = false;
-    private Vector3 playerCheckpoint;
-    private Vector3 _startPosLvl;
     const string settingsSaveDataPath = "/SettingsSaveData.dat";
 
     // Start is called before the first frame update
@@ -25,22 +22,15 @@ public class GameManager : MonoBehaviour
         if (Instance != null && Instance != this)
         {
             // There's already an instance, destroy this one
-            Debug.Log("Deletei");
-            init();
             Destroy(gameObject);
+            Debug.Log("Deletei");
         }
         else
         {
             Instance = this;
             isInstance = true;
-            _startPosLvl = startPosObj.transform.position;
             DontDestroyOnLoad(gameObject.transform.root.gameObject);
         }
-    }
-
-    private void init()
-    {
-        HandlePlayerCheckpoint();
     }
 
     public void SaveSettingsData()
@@ -55,7 +45,6 @@ public class GameManager : MonoBehaviour
     {
         if (File.Exists(Application.persistentDataPath + settingsSaveDataPath))
         {
-            Debug.Log("Entrei no init no GameManager");
             BinaryFormatter bf = new BinaryFormatter();
             FileStream settingsSaveFile = File.Open(Application.persistentDataPath + settingsSaveDataPath, FileMode.Open);
             settingsData = (SettingsData)bf.Deserialize(settingsSaveFile);
@@ -65,30 +54,5 @@ public class GameManager : MonoBehaviour
         {
             SaveSettingsData();
         }
-    }
-
-    void HandlePlayerCheckpoint()
-    {
-        // Find the player object only if it's not assigned in the Inspector
-        if (Instance.playerObject == null && Instance.isInstance == true)
-        {
-            Instance.playerObject = GameObject.FindGameObjectWithTag("Player");
-        }
-
-        // Initialize the player's position if a checkpoint is available
-        if (Instance.playerCheckpoint != new Vector3(0, 0, 0) && Instance.isInstance == true)
-        {
-            Debug.Log(Instance.playerCheckpoint);
-            Instance.playerObject.transform.position = Instance.playerCheckpoint;
-        }
-        else if (Instance.playerCheckpoint == new Vector3(0, 0, 0) && Instance.isInstance == true)
-        {
-            Debug.Log(Instance._startPosLvl);
-            Instance.playerObject.transform.position = Instance._startPosLvl;
-        }
-    }
-    public void ChangeCheckpointPosition(Transform newPosition)
-    {
-        playerCheckpoint = newPosition.position;
     }
 }
