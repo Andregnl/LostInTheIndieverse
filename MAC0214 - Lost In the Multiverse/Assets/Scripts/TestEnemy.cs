71336604bed5895420b5dea13615758749f6c798
Gabriel Alves <alvesgabriel@usp.br>
2023-11-07 11:26:43 -0300

    added class and edited script in enemiy to collide

--- MAC0214 - Lost In the Multiverse/Assets/Scripts/TestEnemy.cs
+++ MAC0214 - Lost In the Multiverse/Assets/Scripts/TestEnemy.cs
@@ -20,4 +20,22 @@ public class TestEnemy : EnemyBasics
 			currentTime = 0.0f;
 		}
     }
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (!col.CompareTag("Base")) return;
+
+		EnemyBasics thatEnemy = col.GetComponent<EnemyBasics>();
+		if (thatEnemy == null)
+		{
+			Debug.LogError("Base does not have base Script!");
+			return;
+		}
+
+		if (thatEnemy.GetRow() != row) return;
+		Debug.Log("Projectile hit something" + col.gameObject.name + " " + col.gameObject.tag);
+		
+
+		collidedEnemies.Enqueue(thatEnemy);
+	}
 }
