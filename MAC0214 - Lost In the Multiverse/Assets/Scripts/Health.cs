3ca085e5b48a9e135c75c5da720cc6170826830a
André Gustavo Nakagomi Lopez <andregnl@usp.br>
2023-11-15 18:16:48 -0300

    Add healthbar to base
    Signed-off-by: André Gustavo Nakagomi Lopez <andregnl@usp.br>

--- MAC0214 - Lost In the Multiverse/Assets/Scripts/Health.cs
+++ MAC0214 - Lost In the Multiverse/Assets/Scripts/Health.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
 
     [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float health = 100f;
+	[SerializeField] Animator animator;
+	[SerializeField] Image healthbar;
 
     public float GetCurrentHealth(){
         return health;
@@ -21,10 +24,23 @@ public class Health : MonoBehaviour
     public void TakeDamage(float damage){
         float newHealth = health - damage;
         if(newHealth <= 0) Die();
-        else health -= damage;
+        else
+		{
+			health -= damage;
+
+			if (healthbar != null)
+				healthbar.fillAmount = health / maxHealth;
+
+			if (animator != null)
+				animator.SetTrigger("TakeDamage");
+		}
     }
 
     public void Die(){
+
+		if (healthbar != null)
+			healthbar.fillAmount = 0;
+
         Object.Destroy(gameObject);
     }
 }
