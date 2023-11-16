674600b3294687dcd8b06aa9c0d94da06ace4d36
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2023-11-16 14:16:21 -0300

    Fixed shieldbug and base animator

--- MAC0214 - Lost In the Multiverse/Assets/ShieldBugHealth.cs
+++ MAC0214 - Lost In the Multiverse/Assets/ShieldBugHealth.cs
@@ -6,19 +6,21 @@ public class ShieldBugHealth : Health
 {
 
     [SerializeField] private int shieldHits = 3;
+
     public override void TakeDamage(float damage){
         
         float newHealth = health - damage;
         
         if (shieldHits <= 0)
         {
-            healthAnimator.SetTrigger("attack");
             if(newHealth <= 0) Die();
             else health -= damage;
         
         }
         else{
             shieldHits -= 1;
+            if(shieldHits == 0)
+                healthAnimator.SetTrigger("TakeDamage");
         }
     }
 }
