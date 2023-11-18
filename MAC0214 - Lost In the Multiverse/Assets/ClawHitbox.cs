27facc07be36c2d1a24741947431b09c27294ddf
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2023-11-18 16:32:42 -0300

    Add Crab enemy basics

---
+++ MAC0214 - Lost In the Multiverse/Assets/ClawHitbox.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawHitbox : MonoBehaviour
+{
+	[SerializeField] GameObject flippableObjects;
+	[SerializeField] float speed;
+	[SerializeField] AudioClip collisionClip;
+    [SerializeField] protected Material _flash;
+    [SerializeField] protected SpriteRenderer sp;
+    private Material originalMaterial;
+	private AudioSource audioSource;
+
+	int row = -1;
+	int damage;
+
+	void Awake()
+	{
+		audioSource = GameObject.
+			FindGameObjectWithTag("ProjectileAudioSource").
+			GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (!col.CompareTag("Enemy")) return;
+
+		EnemyBasics thatEnemy = col.GetComponent<EnemyBasics>();
+		if (thatEnemy == null)
+		{
+			Debug.LogError("Enemy does not have Enemy Script!");
+			return;
+		}
+
+		if (thatEnemy.GetRow() != row) return;
+		Debug.Log("Projectile hit something" + col.gameObject.name + " " + col.gameObject.tag);
+		
+        thatEnemy.GetComponent<Health>().TakeDamage(damage);
+	}
+
+
+	public virtual void SetParameters(int damage, Direction direction, int row)
+	{
+		this.damage = damage;
+		this.row = row;
+
+		/*if (direction == Direction.LEFT)
+		{
+			speed *= -1;
+			flippableObjects.transform.Rotate(new Vector3(0.0f, -180.0f, 0.0f));
+			this.diretion = Direction.LEFT;
+		}
+		else
+		{
+			this.diretion = Direction.RIGHT;
+		}*/
+	}
+	
+}
